f9e107c8fcaec5c2fadd259d4a0d6e5d3508cc93
Kristine <kristinka89@gmail.com>
2014-07-02 18:16:35 +0300

    Add devise and sign_in page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,16 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  layout :layout_by_controller
 
   def tasks_order
-  	Task.order('created_at desc').limit(10)
+    Task.order('created_at desc').limit(10)
+  end
+
+  protected
+
+  def layout_by_controller
+    devise_controller? ? 'devise' : 'application'
   end
 
 end

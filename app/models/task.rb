29e9a013b46ea8171184e62a87284613bac87d70
Kristine <kristinka89@gmail.com>
2014-07-21 15:58:55 +0300

    Add associations

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,10 @@
 class Task < ActiveRecord::Base
+
+  belongs_to :user
+
   validates :note, presence: true
   validate :future_completed_date
+  validate :user, presence: true
 
   def future_completed_date
     if !completed.blank? && completed > Date.today
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140702141519) do
+ActiveRecord::Schema.define(version: 20140721125251) do
 
   create_table "tasks", force: true do |t|
     t.text     "note"
     t.date     "completed"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|

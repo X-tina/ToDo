9285ad9df502ec25f5253479e4113766642bee79
Kristine <kristinka89@gmail.com>
2014-09-01 15:00:56 +0300

    change layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,12 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
-
-  layout :layout_by_controller
+  
+  layout Proc.new { |controller| controller.devise_controller? ? "devise" : "application" }
 
   #don't need anymore
   def tasks_order
     Task.accessible_by(current_ability).order('created_at desc').limit(10)
-  end
-
-  protected
-
-  def layout_by_controller
-    devise_controller? ? 'devise' : 'application'
   end
 
 end
